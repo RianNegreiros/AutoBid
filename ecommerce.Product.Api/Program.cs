28eaa574320a4f7792ca279fcfe35a0b15022f6f
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-09-29 17:19:48 -0300

    build: inject repository and service

--- ecommerce.Product.Api/Program.cs
+++ ecommerce.Product.Api/Program.cs
@@ -1,3 +1,7 @@
+using ecommerce.Infrastructure.Mongo;
+using ecommerce.Product.Api.Repositories;
+using ecommerce.Product.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMongoDB(builder.Configuration);
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
