50fd82ba8a784ee980a0d71dc5bfb6a5718f09a4
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-10-07 16:16:17 -0300

    chore: add ef core with postgresql

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,11 +1,15 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AuctionDbContext>(options =>
+{
+  options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
