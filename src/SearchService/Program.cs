ec513d2ef082a586144703b71a7b49657b43358b
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-10-08 11:49:19 -0300

    chore: add and config rabbitmq

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,13 +1,17 @@
-using MongoDB.Driver;
-using MongoDB.Entities;
+using MassTransit;
 using SearchService.Data;
-using SearchService.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
