c7b1b3e981d91bdadf50118be8c73b32ac936a19
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-04-03 17:41:05 -0300

    chore: add next app client

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -8,10 +8,10 @@ builder.Services.AddReverseProxy()
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
-      options.Authority = builder.Configuration["IdentityServiceUrl"];
-      options.RequireHttpsMetadata = false;
-      options.TokenValidationParameters.ValidateAudience = false;
-      options.TokenValidationParameters.NameClaimType = "username";
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
     });
 
 var app = builder.Build();
