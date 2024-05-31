9ce02e9739c23b2b7039eba5c96315552b12179c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-05-31 11:43:34 -0300

    chore: add bid service to dockerfiles

--- src/BidService/Controllers/BidsController.cs
+++ src/BidService/Controllers/BidsController.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using BidService.DTOs;
 using BidService.Models;
+using Contracts;
+using MassTransit;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
@@ -9,9 +11,10 @@ namespace BidService.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class BidsController(IMapper mapper) : ControllerBase
+public class BidsController(IMapper mapper, IPublishEndpoint publishEndpoint) : ControllerBase
 {
   private readonly IMapper _mapper = mapper;
+  private readonly IPublishEndpoint _publishEndpoint = publishEndpoint;
 
   [Authorize]
   [HttpPost]
@@ -59,6 +62,8 @@ public class BidsController(IMapper mapper) : ControllerBase
     }
 
     await DB.SaveAsync(bid);
+
+    await _publishEndpoint.Publish(_mapper.Map<BidPlaced>(bid));
 
     return Ok(_mapper.Map<BidDto>(bid));
   }
